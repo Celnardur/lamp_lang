c5a4e269bcc0c46f16a20bc4a2c35151c847a07b
Celnardur <celnardur@protonmail.com>
2022-04-18 23:44:39 -0500

    Rearanged the lib order a little

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-pub mod code;
-//pub mod grouper;
-pub mod data_type;
-pub mod map;
-pub mod parse;
 pub mod queue;
 pub mod token;
+pub mod map;
+pub mod code;
+pub mod parse;
+//pub mod grouper;
+pub mod data_type;
+
